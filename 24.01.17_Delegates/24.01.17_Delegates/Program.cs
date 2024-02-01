55d24b1cf20b4e27a4cb9a8c3fcb7c3cf0e21293
Alina <nika23888@gmail.com>
2024-02-01 10:55:13 +0200

    HW

---
+++ 24.01.17_Delegates/24.01.17_Delegates/Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace _24._01._17_Delegates
+{
+    internal class Program
+    {
+        public delegate double CalculationOperation(object[] array);
+        public delegate void ModificationOperation(ref object[] array);
+
+        public class ArrayWork
+        {
+            void Swap(ref object[] array, int index1, int index2)
+            {
+                object temp = array[index1];
+                array[index1] = array[index2];
+                array[index2] = temp;
+            }
+            bool IsPrime(double number)
+            {
+                try
+                {
+                    if (number <= 1 || number % 1 != 0)
+                        return false;
+
+                    int intNumber = (int)number;
+
+                    for (int i = 2; i <= Math.Sqrt(intNumber); i++)
+                    {
+                        if (intNumber % i == 0)
+                            return false;
+                    }
+
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            public static void Show(object[] array)
+            {
+                Console.WriteLine("Array: ");
+
+                foreach (var element in array)
+                {
+                    Console.Write($"[{element}] ");
+                }
+
+                Console.WriteLine();
+            }
+
+            public double NegativeElements(object[] array)
+            {
+                return array.Count(x => (double)x < 0);
+            }
+            public double PrimeNumbers(object[] array)
+            {
+                return array.Count(x => IsPrime((double)x));
+            }
+            public double SumElements(object[] array)
+            {
+                return array.Sum(x => (double)x);
+            }
+
+            public void ChangeNegativeToZero(ref object[] array)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if ((double)array[i] < 0) array[i] = 0;
+                }
+            }
+            public void Sort(ref object[] array)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    for (int j = 0; j < array.Length - 1; j++)
+                    {
+                        if (IsPrime((double)array[j]) && !IsPrime((double)array[j + i])) { Swap(ref array, j, j + 1); }
+                        else if (IsPrime((double)array[j]) == IsPrime((double)array[j + 1]))
+                        {
+                            if (Convert.ToDouble(array[j]) > Convert.ToDouble(array[j + 1])) { Swap(ref array, j, j + 1); }
+                        }
+                    }
+                }
+            }
+        }
+        static public class Menu
+        {
+            static public int StartMenu()
+            {
+                try
+                {
+                    Console.WriteLine("|=|=|=|   MENU   |=|=|=|");
+                    Console.WriteLine("[0] - Exit");
+                    Console.WriteLine("[1] - Calculate");
+                    Console.WriteLine("[2] - Change");
+                    Console.WriteLine();
+
+                    Console.Write("(/^.^)/~  "); int res = int.Parse(Console.ReadLine());
+                    return res;
+                }
+                catch (Exception ex) { throw ex; }
+            }
+            static public int CalculateMenu()
+            {
+                Console.WriteLine("|=|=|=|   CALCULATE MENU   |=|=|=|");
+                Console.WriteLine("[0] - Exit");
+                Console.WriteLine("[1] - Calculate the number of negative elements");
+                Console.WriteLine("[2] - Determine the sum of all elements");
+                Console.WriteLine("[3] - Calculate the number of primes");
+                Console.WriteLine();
+
+                Console.Write("(/^.^)/~  "); int res = int.Parse(Console.ReadLine());
+                return res;
+            }
+            static public int ChangeMenu()
+            {
+                Console.WriteLine("|=|=|=|   CHANGE MENU   |=|=|=|");
+                Console.WriteLine("[0] - Exit");
+                Console.WriteLine("[1] - Change all negative elements to 0");
+                Console.WriteLine("[2] - Sort the array");
+                Console.WriteLine("[3] - Move all even elements to the beginning, respectively, odd elements will be at the end");
+                Console.WriteLine();
+
+                Console.Write("(/^.^)/~  "); int res = int.Parse(Console.ReadLine());
+                return res;
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            /*                
+                    Написати програму для виконання операцій з одновимірним масивом за допомогою делегатів.                
+                
+                    Користувачу надається наступне меню для вибору типа операції з масивом:                    
+                        1. обчислення значення                    
+                        2. зміна масиву
+                    
+                    Якщо користувач вибрав 1-й тип, вивести підменю вибору операції:
+                            1. Обчислити кількість негативних елементів
+                            2. Визначити суму всіх елементів
+                            3. *Обрахувати кількість простих чисел
+
+                    Якщо користувач вибрав 2-й тип, вивести підменю вибору операції:
+                            1. Змінити всі негативні елементи на 0
+                            2. Відсортувати масив
+                            3. *Перемістити всі парні елементи на початку, відповідно не парні будуть в кінці.
+                    
+                    Створити вказані вище методи та реалізувати вибір користувачем операції на виконання 
+                    без використання конструкцій if, switch та ?:(тернарного) оператора.
+                                
+                    Методи першого типу повинні повертати результат обчислення, в той час методи другого типу – void.
+                    Реалізувати валідацію вибраного номера операції. 
+            */
+
+
+            ArrayWork aw = new ArrayWork();
+            int[] array = { -2, 5, 8, -3, 0, 7, 10, -1 };
+
+            Random rand = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                int randomValue = rand.Next(1, 20);
+                array.Append(randomValue);
+            }
+
+            CalculationOperation co_delegate = aw.NegativeElements;
+            co_delegate += aw.PrimeNumbers;
+            co_delegate += aw.SumElements;
+
+            ModificationOperation mo_delegate = aw.ChangeNegativeToZero;
+            mo_delegate = aw.Sort;
+            
+        }
+    }
+}
---
+++ 24.01.31_Attributes_Serialization/24.01.31_Attributes_Serialization/Program.cs
@@ -0,0 +1,96 @@
+﻿using System.Text.Json;
+
+namespace _24._01._31_Attributes_Serialization
+{
+    internal class Program
+    {
+        [Serializable]
+        public class Person
+        {
+            public Person() {  }
+            public Person(int number) { _identNumber = number; }
+            
+            [NonSerialized]
+            const string Planet = "Earth";
+
+            public string Name { get; set; }
+            public int Age { get; set; }
+
+            public int _identNumber;            
+            public override string ToString()
+            {
+                return $"Name : [{Name}] | Age: [{Age}] | Identification number : [{_identNumber}] | Planet : [{Planet}]";
+            }
+        }
+        static void Main(string[] args)
+        {
+            /*            
+            1. Реалізувати додаток з наступним функціоналом:
+                a)Консольне меню
+                b)В якості колекції для даних використати Словник (Dictionary<TKey, TValue>), який реалізує CRUD
+                c) Значущими елементами словника є екземпляри класу User
+
+            2. Класс User повинен містити наступні властивості:
+                a) Id - int унікальні значення в діапазоні 1000 - 9999
+                b) Login - string, лише друємі символи без спец сиволів
+                c) Password - string, лише друємі символи без спец сиволів, довжина не менше 8ми сиволів,
+                d) ConfirmPassword - string, лише друємі символи без спец сиволів, довжина не менше 8ми сиволів,
+                e) E-mail - string, валідація згідно загальних правил стандарту
+                f) CreditCard - валідація згідно загальних правил стандарту
+                g)Phone - валідація згідно українського формату +38-0**-***-**-**
+
+            3. Всі властивості містять відповідні атрибути валідації, з перевизначиними повідомленнями, 
+                згідно яких модель після перевірки записується в словник . 
+                Якщо якісь властивості не проходять валідацію - користувач змушений ввести їх повторно.     
+            
+            4. Після вибору відповідного пункту меню екземпляр словника серіалізується і зберігається у файл. (робиться резервна копія)                
+            
+            5. Після вибору відповідного пунктуц меню дані з файлу читаються і десеріалізуються переписуючи поточний екземпляр.
+                
+            Bonus 12: при десеріалізації даних вмісти 2ох словників порівнюються і користувачеві пропонується наступні дії:
+                1. Переписати весь вміст
+                2. Дописати в пам”ять лише ті дані Користувачів яких там не вистачає ( якщо такі є). 
+                    Якщо користувач є в пам”яті в файлі, користувач може вибрати 
+                    ячку версію даних прийняти а яку відкинути.
+            */
+/*
+            List<Person> people = new List<Person>()
+            {
+                 new Person(1000){ Name = "Jack", Age = 25},
+                 new Person(2000){ Name = "Bob", Age = 15},
+                 new Person(3000){ Name = "Tom", Age = 45}
+            };
+            foreach (var item in people)
+            {
+                Console.WriteLine(item);
+                Console.WriteLine();
+            }*/
+
+            try
+            {
+                //Serialize
+                /*string fileName = "Persons.json";
+                string jsonString = JsonSerializer.Serialize(people);*/
+                //File.WriteAllText(fileName, jsonString);
+
+                string fileName = "Persons.json";
+                string jsonString;
+
+                //Deserialize
+                jsonString = File.ReadAllText(fileName);
+
+                List<Person> list = JsonSerializer.Deserialize<List<Person>>(jsonString);
+                foreach (var item in list)
+                {
+                    Console.WriteLine(item);
+                    Console.WriteLine();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
